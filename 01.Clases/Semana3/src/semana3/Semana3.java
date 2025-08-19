a3ba5ad994b4de2a174f43fe6055db518c555e23
pgaticac <pamela.gatica@usm.cl>
2025-08-19 12:05:00 -0400

    Proyectos Java - Hola Mundo

---
+++ 01.Clases/Semana3/src/semana3/Semana3.java
@@ -0,0 +1,8 @@
+package semana3;
+
+public class Semana3 {
+    public static void main(String[] args) {        //psvm
+        System.out.println("Hola Mundo");                     //sout    
+    }
+    
+}
