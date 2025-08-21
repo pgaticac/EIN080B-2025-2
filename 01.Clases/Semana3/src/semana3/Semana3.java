6b3227c2e5e88e16d2f470880ea7515a1119984c
pgaticac <pamela.gatica@usm.cl>
2025-08-21 12:04:02 -0400

    Proyectos Java - Variables y Casting

--- 01.Clases/Semana3/src/semana3/Semana3.java
+++ 01.Clases/Semana3/src/semana3/Semana3.java
@@ -1,8 +1,17 @@
 package semana3;
 
 public class Semana3 {
-    public static void main(String[] args) {        //psvm
-        System.out.println("Hola Mundo");                     //sout    
+    public static void main(String[] args) {              //psvm
+        System.out.println("Hola Mundo");                 //sout    
+        byte dia = 21;
+        short año = 2025;
+        
+        //dia =(byte) año;
+        año = dia;
+        
+        System.out.println("Dia: " + dia);
+        System.out.println("Año:" + año);
+        
     }
     
 }
